ffea72c400bc89b244305f8d695a3bfde117c27c
FiveTony <a.malynov@wim.agency>
2022-06-08 12:36:04 +0300

    first

---
+++ src/assets/scripts/classes/Positive.js
@@ -0,0 +1,45 @@
+import MovableObject from './MovableObject'
+import PositiveObject from './PositiveObject'
+
+export default class Positive extends Phaser.Physics.Arcade.Group {
+    constructor(scene, config) {
+        super(scene.physics.world, scene)
+        this.scene = scene
+
+        this.config = config
+
+        this.countMax = this.config.maxPositiveOnLevel
+        this.countCreated = 0
+
+        this.timer = this.scene.time.addEvent({
+            delay: 900,
+            loop: true,
+            callback: this.tick,
+            callbackScope: this
+        })
+
+    }
+    createPositiveObject() {
+        let positiveObject = this.getFirstDead()
+        if (!positiveObject) {
+            positiveObject = PositiveObject.generate(this.scene)
+            MovableObject.lastX_P = positiveObject.x
+            positiveObject.positive = true
+            this.add(positiveObject)
+        } else {
+            positiveObject.reset()
+        }
+
+        positiveObject.move(this.config.velocity, this.config.isHorizontal, this.scene.countScorePositive)
+        if (this.config.isRotation) positiveObject.setRotation()
+        this.countCreated++
+    }
+    tick() {
+        if (this.countCreated < this.countMax) {
+            this.createPositiveObject()
+            this.timer.delay -= this.scene.countScorePositive * this.config.accelerationFrequency
+        } else {
+            this.timer.remove()
+        }
+    }
+}
